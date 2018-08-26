bcaf824d0df59845e7e7b51a1292c12cf3ceee4b
Kent <kentsaeteurn@gmail.com>
2018-08-26 02:20:19 -0700

    fix styles, fix dropdown on mobile view, add navbar partial

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -9,7 +9,7 @@ describe("routes : static", () => {
     it("should return status code 200", (done) => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-        expect(body).toContain("Portfolio");
+        expect(body).toContain("Kent's Portfolio");
         done();
       })
     })
